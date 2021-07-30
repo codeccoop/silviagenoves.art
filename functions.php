e760e687e6ea82ddb93380d80d0136eeda52401f
Pau <pau.pulidocompanys@gmail.com>
2021-07-30 20:01:44 +0200

    canvi padding imatges petites

--- functions.php
+++ functions.php
@@ -307,8 +307,4 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 /*Eliminem els related products*/
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
-remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_login_form', 10 );
-remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
-
-remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 ?>
